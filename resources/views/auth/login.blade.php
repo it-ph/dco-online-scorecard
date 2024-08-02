9bbcbf86ab26a1fd99b8f3164a3c0ca97edec513
PERSONIV\rico.bugtong <rico.gomez.bugtong@gmail.com>
2024-08-03 06:33:39 +0800

    added content security policy header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-
+<?php header("Content-Security-Policy: default-src 'self'; style-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; font-src https://fonts.googleapis.com https://fonts.gstatic.com 'self' data:"); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,147 +20,11 @@
     <link href="{{asset('css/login/login-register-lock.css')}}" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="{{asset('css/login/login.css')}}" rel="stylesheet">
-    <style nonce="{{csp_nonce()}}">
+    <link href="{{asset('css/login/styles.css')}}" rel="stylesheet">
 
-    .login-form-txt{
-        border: 0px !important;
-        border-bottom: 1px solid #d8d8d8 !important;
-    }
+{{-- <style class="cp-pen-styles">
 
-
-    input:focus {
-        border-bottom: 2px solid #1976d2 !important;
-}
-    </style>
-
-<style class="cp-pen-styles" nonce={{csp_nonce()}}>@import    url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300);
-        * {
-          -webkit-box-sizing: border-box;
-                  box-sizing: border-box;
-          margin: 0;
-          padding: 0;
-          font-weight: 300;
-        }
-
-
-        form {
-          padding: 20px 0;
-          position: relative;
-          z-index: 2;
-        }
-
-
-        .bg-bubbles {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          z-index: 1;
-        }
-        .bg-bubbles li {
-          position: absolute;
-          list-style: none;
-          display: block;
-          width: 40px;
-          height: 40px;
-          background-color: rgba(255, 255, 255, 0.15);
-          bottom: -160px;
-          -webkit-animation: square 25s infinite;
-          animation: square 25s infinite;
-          -webkit-transition-timing-function: linear;
-          transition-timing-function: linear;
-        }
-        .bg-bubbles li:nth-child(1) {
-          left: 10%;
-        }
-        .bg-bubbles li:nth-child(2) {
-          left: 20%;
-          width: 80px;
-          height: 80px;
-          -webkit-animation-delay: 2s;
-                  animation-delay: 2s;
-          -webkit-animation-duration: 17s;
-                  animation-duration: 17s;
-        }
-        .bg-bubbles li:nth-child(3) {
-          left: 25%;
-          -webkit-animation-delay: 4s;
-                  animation-delay: 4s;
-        }
-        .bg-bubbles li:nth-child(4) {
-          left: 40%;
-          width: 60px;
-          height: 60px;
-          -webkit-animation-duration: 22s;
-                  animation-duration: 22s;
-          background-color: rgba(255, 255, 255, 0.25);
-        }
-        .bg-bubbles li:nth-child(5) {
-          left: 70%;
-        }
-        .bg-bubbles li:nth-child(6) {
-          left: 80%;
-          width: 120px;
-          height: 120px;
-          -webkit-animation-delay: 3s;
-                  animation-delay: 3s;
-          background-color: rgba(255, 255, 255, 0.2);
-        }
-        .bg-bubbles li:nth-child(7) {
-          left: 32%;
-          width: 160px;
-          height: 160px;
-          -webkit-animation-delay: 7s;
-                  animation-delay: 7s;
-        }
-        .bg-bubbles li:nth-child(8) {
-          left: 55%;
-          width: 20px;
-          height: 20px;
-          -webkit-animation-delay: 15s;
-                  animation-delay: 15s;
-          -webkit-animation-duration: 40s;
-                  animation-duration: 40s;
-        }
-        .bg-bubbles li:nth-child(9) {
-          left: 25%;
-          width: 10px;
-          height: 10px;
-          -webkit-animation-delay: 2s;
-                  animation-delay: 2s;
-          -webkit-animation-duration: 40s;
-                  animation-duration: 40s;
-          background-color: rgba(255, 255, 255, 0.3);
-        }
-        .bg-bubbles li:nth-child(10) {
-          left: 90%;
-          width: 160px;
-          height: 160px;
-          -webkit-animation-delay: 11s;
-                  animation-delay: 11s;
-        }
-        @-webkit-keyframes square {
-          0% {
-            -webkit-transform: translateY(0);
-                    transform: translateY(0);
-          }
-          100% {
-            -webkit-transform: translateY(-700px) rotate(600deg);
-                    transform: translateY(-700px) rotate(600deg);
-          }
-        }
-        @keyframes    square {
-          0% {
-            -webkit-transform: translateY(0);
-                    transform: translateY(0);
-          }
-          100% {
-            -webkit-transform: translateY(-700px) rotate(600deg);
-                    transform: translateY(-700px) rotate(600deg);
-          }
-        }
-        </style>
+        </style> --}}
 
     <!-- You can change the theme colors from here -->
     {{-- <link href="css/colors/default-dark.css" id="theme" rel="stylesheet"> --}}
@@ -176,17 +40,17 @@
     <!-- ============================================================== -->
     <!-- Preloader - style you can find in spinners.css -->
     <!-- ============================================================== -->
-    <div class="preloader">
+    {{-- <div class="preloader">
         <div class="loader">
             <div class="loader__figure"></div>
             <p class="loader__label">DCO Scorecard</p>
         </div>
-    </div>
+    </div> --}}
     <!-- ============================================================== -->
     <!-- Main wrapper - style you can find in pages.scss -->
     <!-- ============================================================== -->
     {{-- <section id="wrapper" class="login-register login-sidebar" style="background-image:url({{asset('images/login.jpg')}});"> --}}
-        <section id="wrapper" class="login-register login-sidebar" style="background-image:url({{asset('images/login.jpg')}}); background: #003b5d">
+        <section id="wrapper" class="login-register login-sidebar">
             <ul class="bg-bubbles">
                 <li></li>
                 <li></li>
@@ -204,8 +68,8 @@
                 <!-- <form method="POST" action="{{ route('login') }}"> -->
                 <form>
                     @csrf
-                    <a href="javascript:void(0)" class="text-center db"><img src="{{asset('images/personiv/logo.png')}}" style="width: 50px;color: black" alt="Home" /><br/>
-                        <span style="color: black; font-weight: bold">  DCO SCORECARD</span>
+                    <a href="javascript:void(0)" class="text-center db"><img src="{{asset('images/personiv/logo.png')}}" class="logo" alt="Home" /><br/>
+                        <span class="dco-scorecard">  DCO SCORECARD</span>
                     </a>
                     <!-- <div class="form-group m-t-40">
                         <div class="col-xs-12">
@@ -280,23 +144,6 @@
     <!-- Bootstrap tether Core JavaScript -->
     <script src="{{asset('js/theme/popper.min.js')}}"></script>
     <script src="{{asset('js/theme/bootstrap.min.js')}}"></script>
-    <!--Custom JavaScript -->
-    <script nonce="{{csp_nonce()}}" type="text/javascript">
-        $(function() {
-            $(".preloader").fadeOut();
-        });
-        $(function() {
-            $('[data-toggle="tooltip"]').tooltip()
-        });
-        // ==============================================================
-        // Login and Recover Password
-        // ==============================================================
-        $('#to-recover').on("click", function() {
-            $("#loginform").slideUp();
-            $("#recoverform").fadeIn();
-        });
-    </script>
-
 </body>
 
 </html>
